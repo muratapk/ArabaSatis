51b76506ec7eb83971f1b7f32e86f3a15c93b4bf
muratapk <muratapk@hotmail.com>
2025-06-27 21:33:03 +0300

    ilanlar eklenmesi yapıldı

---
+++ ArabaSatis/Component/KategoriListe.cs
@@ -0,0 +1,20 @@
+﻿using ArabaSatis.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArabaSatis.Component
+{
+    public class KategoriListe:ViewComponent
+    {
+        private readonly ArabamDbContext _context;
+        public KategoriListe(ArabamDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            
+            return View();  
+        }
+
+    }
+}
---
+++ ArabaSatis/Component/VitrinListe.cs
@@ -0,0 +1,22 @@
+﻿using ArabaSatis.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArabaSatis.Component
+{
+    
+    public class VitrinListe:ViewComponent
+    {
+        private readonly ArabamDbContext _context;
+
+        public VitrinListe(ArabamDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var sorgu = _context.Ilanlars.ToList();
+            return View(sorgu);
+        }
+    }
+}
